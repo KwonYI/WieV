7372eff82f5395d7113cfc3fc20855086067408e
dooly7183 <dooly7183@naver.com>
2021-02-04 14:12:45 +0900

    F_엑셀다운받기_FIN

--- frontend/src/views/viewset/Viewers.vue
+++ frontend/src/views/viewset/Viewers.vue
@@ -1,5 +1,21 @@
 <template>
   <div id="viewers">
+    <v-toolbar dark color="teal">
+      <v-toolbar-title>면접관 찾기</v-toolbar-title>
+      <v-autocomplete v-model="select" :loading="loading" :items="items" :search-input.sync="search" cache-items
+        class="mx-4" flat hide-no-data hide-details label="지원자 이름 검색" solo-inverted></v-autocomplete>
+      <v-btn class="m-3"> 검색 </v-btn>
+
+        <label>File
+          <input v-model="title">
+          <input type="file" id="files" ref="files" v-on:change="handleFileUpload()" multiple />
+        </label>
+        <v-btn class="m-3" v-on:click="submitFile()">엑셀 업로드</v-btn>
+    
+      <v-btn class="m-3" @click="exportExcel">엑셀 양식 다운로드</v-btn>
+    
+      <!-- <v-btn class="m-3" @click="createVieweeDB"> DB업데이트 </v-btn> -->
+    </v-toolbar>
     <v-simple-table fixed-header  height="500px" class="mt-5">
       <thead>
         <tr>
@@ -29,12 +45,25 @@
 
 <script>
   import { mapGetters, mapState } from "vuex"
-
+import XLSX from 'xlsx'
+  import axios from 'axios'
+  const SERVER_URL = "https://localhost:8080"
 
   export default {
     name: "Viewers",
     data: function () {
       return {
+         tableData: [{
+        순서:'1',
+        이름: '홍길동(예시)',
+        이메일:'email@gmail.com',
+        부서:'CE/IM',
+        직군:'SW개발',
+        핸드폰번호:'010-1111-2222',
+        관리자OR면접관:'면접관',
+      }],
+        title: "",
+        files: [],
         reno: "",
 
       }
@@ -44,7 +73,46 @@
       this.reno = this.$store.state.selectedRecruitNo
       console.log("reno:", this.reno)
     },
+    methods:{
+      exportExcel() {
+      const wb = XLSX.utils.book_new()    // 엑셀 파일 생성 (workbook)
+      const ws = XLSX.utils.json_to_sheet(this.tableData)    // 시트 생성 (worksheet) 및 데이터 삽입
+      
+      XLSX.utils.book_append_sheet(wb, ws, 'sheet1')  // 엑셀 파일에 시트 추가
 
+      XLSX.writeFile(wb, '엑셀양식.xlsx') // 엑셀 다운로드
+    },
+      filterdVieweeList: function () { 
+        return this.comVieweeList.filter(re => re.recruitReSeq === this.reno)
+
+      },
+
+      submitFile() {
+
+        let formData = new FormData();
+        formData.append('title', this.title);
+        formData.append('files', this.files[0]);
+        console.log(this.reno)
+        axios.post(`${SERVER_URL}/interviewer/register/` + this.reno,
+            formData, {
+              headers: {
+                'Content-Type': 'multipart/form-data'
+              }
+            }
+          ).then(function () {
+            console.log('SUCCESS!!');
+          })
+          .catch(function () {
+            console.log('FAILURE!!');
+          });
+
+      },
+
+      handleFileUpload() {
+        this.files = this.$refs.files.files;
+        console.log(this.files);
+      }
+    },
     computed: {
       ...mapState(["selectedRecruitNo", "recruitList", "comViewerList"]),
       ...mapGetters(["getComViewerList"])
